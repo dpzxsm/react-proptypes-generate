ae74196f4a80ae46047a17ff5319e1dda286818f
suming <dpzxsm@sina.com>
2018-11-21 17:58:16 +0800

    resume

--- publish.js
+++
@@ -1,5 +0,0 @@
-let envKeys = Object.keys(process.env);
-if (envKeys.some(item => item.indexOf("VSCODE") >= 0)) {
-  console.log('suming-log', 'prepare install');
-  require('vscode/bin/install');
-}
