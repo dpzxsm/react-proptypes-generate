8d322b655c7a7f1afd147e7cc78862a5fcf2d16e
suming <dpzxsm@sina.com>
2021-01-26 11:05:13 +0800

    整理example文件

---
+++ examples/child/index2.js
@@ -0,0 +1,27 @@
+const Test = React.memo((props) => {
+	let { school: schoolAlias = "schoolName", info = { name: "test" }, age = 33, students = [] } = props;
+	if (info.year === 2020) {
+		console.log("will generate year type as number");
+	}
+	// support deep find
+	let childInfo = info.child.info;
+	// support null js null propagation
+	return <div onClick={() => props?.onClick()}>
+		{students.map((student, index) => <div key={index}>{item.name}</div>)}
+	</div>;
+});
+
+Test.Item1 = function (props) {
+	return <div>
+		{props.name}
+	</div>;
+};
+
+Test.Item2 = React.memo(function (props) {
+	return <div>
+		{props.name}
+	</div>;
+});
+
+export
+Test;
--- examples/index.jsx
+++ examples/index.jsx
@@ -1,25 +1,28 @@
-import PropTypes from "prop-types";
-export function Test(props) {
+export class Test extends Component {
+  render() {
+    let { school: schoolAlias = "schoolName", info = { name: "test" }, age = 33, students = [] } = this.props;
+    if(info.year === 2020){
+      console.log("will generate year type as number");
+    }
+    // support deep find
+    let childInfo = info.child.info;
+    // support null js null propagation
+    return <div onClick={() => this.props?.onClick()}>
+      {students.map((student, index) =><div key={index}>{item.name}</div>)}
+    </div>
+  }
+}
+
+export default function Test2(props){
   let { school: schoolAlias = "schoolName", info = { name: "test" }, age = 33, students = [] } = props;
   if(info.year === 2020){
     console.log("will generate year type as number");
   }
+  // support deep find
   let childInfo = info.child.info;
+  // support null js null propagation
   return <div onClick={() => props?.onClick()}>
     {students.map((student, index) =><div key={index}>{item.name}</div>)}
   </div>
 }
 
-Test.propTypes = {
-  age: PropTypes.number.isRequired, // age
-  info: PropTypes.shape({
-    child: PropTypes.shape({
-      info: PropTypes.any, // info.child.info
-    }),
-    name: PropTypes.string,
-    year: PropTypes.number, // info.year
-  }).isRequired, // info
-  onClick: PropTypes.func.isRequired,
-  school: PropTypes.string.isRequired,
-  students: PropTypes.array.isRequired, // students
-}
